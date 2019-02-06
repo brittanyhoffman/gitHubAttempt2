4b37a3bae14f2e5e91fa0868c660aebbe9f67303
brittanyhoffman1308@gmail.com <>
2019-02-06 10:57:11 -0500

    This is the second commit

---
+++ src/githubattempt2/GitHubAttempt2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubattempt2;
+
+/**
+ *
+ * @author Brittany Hoffman
+ */
+public class GitHubAttempt2 {
+
+    public static void main(String[] args) 
+    {
+        Hoffman();
+    }
+    public static void Hoffman()
+    {
+        System.out.println("Brittany Hoffman!!");
+    }
+}
